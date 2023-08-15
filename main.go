cd4287d5b319a333b480d36c91206231daf827d6
Gitux1602 <gdelmercado@outlook.com>
2023-08-15 16:05:48 -0600

    Condicionales

--- main.go
+++ main.go
@@ -1,11 +1,27 @@
 package main
 
 import (
+	"fmt"
 	"godesde0/variables"
+	"runtime"
 )
 
 func main() {
 	variables.RestoVariables()
 	variables.ConviertoaTexto(100)
 
+	if os := runtime.GOOS; os == "Linux." || os == "OS X." {
+		fmt.Println("Esto no es Windows")
+	} else {
+		fmt.Println("Estas en Windows")
+	}
+
+	switch os := runtime.GOOS; os {
+	case "linux":
+		fmt.Println("Esto es Linex")
+	case "darwin":
+		fmt.Println("Darwin")
+	default:
+		fmt.Printf("Operative System: %s", os)
+	}
 }
