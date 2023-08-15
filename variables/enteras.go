8155f9d7b77010bc86c0222fb2e3ff784c710b0b
Gitux1602 <gdelmercado@outlook.com>
2023-08-15 11:18:17 -0600

    Inicio con variables e importacion de paquetes

---
+++ variables/enteras.go
@@ -0,0 +1,14 @@
+package variables
+
+import "fmt"
+
+func MUESTROENTEROS() {
+
+	var x int
+	y := int32(10)
+	z := int64(10)
+
+	fmt.Println("X = ", x)
+	fmt.Println("Y = ", y)
+	fmt.Println("Z = ", z)
+}
